4a84b830b833614b64434b4361eef8df8e3f697f
amarifields <sumbusinessinquires@gmail.com>
2026-01-07 23:54:05 -0500

    Fix JSX syntax error in Favorites.tsx - replace closing section tag with div and Card

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -425,7 +425,8 @@ const Favorites = () => {
                       onToggleFavorite={toggleCEFFavorite}
                     />
                   </div>
-                </section>
+                  </div>
+                </Card>
               )}
             </>
           )}
